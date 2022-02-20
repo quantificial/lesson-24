c070ff2fd95f8014c15566253cbd2e7fd7c682e3
Johnson Fu <johnson.fu@gmail.com>
2022-02-20 16:34:24 +0800

    update version

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51KVAT7EJxtVdSB2p3sp4jmWgXFeSP77nbV6egjiVL9HhpQXwsagXIiTit1baxuYTmwJAx396KGzMhKeIK7z6OAky00C6Tqf2sG';
 
   const onToken = token => {
     console.log(token);
@@ -12,10 +12,12 @@ const StripeCheckoutButton = ({ price }) => {
 
   return (
     <StripeCheckout
+      locale='zh'
       label='Pay Now'
       name='CRWN Clothing Ltd.'
       billingAddress
       shippingAddress
+      alipay
       image='https://svgshare.com/i/CUz.svg'
       description={`Your total is $${price}`}
       amount={priceForStripe}
